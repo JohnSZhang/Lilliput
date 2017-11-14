4ad9748bd3b6de4b0f68b3ba43bfb8755abe0373
John Zhang <john.shimin.zhang@gmail.com>
2017-11-14 15:17:22 +0700

    add model step code

---
+++ spec/model/mode_spec.js
@@ -0,0 +1,52 @@
+Model = require("../../src/lib/model.js");
+
+describe("A test suite of model tests", function () {
+    var model;
+
+    beforeAll(function () {
+    });
+
+    beforeEach(function () {
+        model = new Model()
+    });
+
+    it('has correct initial step', function () {
+        expect(model.step_count).toEqual(0);
+    });
+
+    it('steps correctly', function () {
+        for (let i = 0; i < 5; i++){
+            model.step();
+        }
+        expect(model.step_count).toEqual(5)
+    });
+
+    it('stops correctly on max step', function () {
+        model.max_step = 10;
+        model.step_til_end();
+        expect(model.step_count).toEqual(10);
+    });
+
+    it('stops correctly on stop function', function(){
+        model.max_step = 1000;
+        let stop_crit = function (model) {
+            if (model.step_count >= 10){
+                return true;
+            }
+            return false;
+        }
+        model.stop_criteria = stop_crit;
+        model.step_til_end();
+        expect(model.step_count).toEqual(10);
+    });
+
+    it('correctly updates agents in sequential mode', function () {
+
+    });
+
+    it ('correctly updates agents in random action mode', function () {
+
+    });
+
+
+});
--- src/lib/model.js
+++ src/lib/model.js
@@ -1,6 +1,35 @@
+let _ = require('lodash');
+
 class Model {
-    constructor(name) {
-        this.name = name;
+    constructor(opt) {
+        let defaults = {
+            max_step: 100,
+            stop_criteria: undefined,
+        };
+        let options = _.defaults(opt, defaults);
+        this.max_step = options.max_step;
+        this.stop_criteria = options.stop_criteria;
+        this.step_count = 0;
+    }
+    step(){
+        this.step_agents();
+        this.step_count += 1;
+    }
+    step_til_end(){
+        let stop = false;
+        if (this.stop_criteria === undefined) {
+            while(this.step_count < this.max_step) {
+                this.step();
+            }
+        } else {
+            while (!stop) {
+                this.step();
+                stop = this.stop_criteria(this);
+            }
+        }
+    }
+    step_agents(){
+        return true;
     }
 };
 
